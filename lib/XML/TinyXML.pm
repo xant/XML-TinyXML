62ee3ac1f2603137e798390398fe341df09f354e
xant <xant@efd22584-a12f-43d1-8b60-7d822078459d>
2008-06-10 13:17:56 +0000

    - fix to extract kbdevents coming from picomod and nanomod so   logged with the 'mod' suffix in the type field
    - allow to build and run XML::TinyXML with perl interpreters < 5.8.8
    
    
    
    git-svn-id: file:///DATA/svn/code@2938 efd22584-a12f-43d1-8b60-7d822078459d

--- lib/XML/TinyXML.pm
+++ lib/XML/TinyXML.pm
@@ -74,7 +74,6 @@ more than 10 modules to have a simple xml implementation.
 
 package XML::TinyXML;
 
-use 5.008008;
 use strict;
 use warnings;
 use Carp;
@@ -134,7 +133,7 @@ our @EXPORT = qw(
 	XmlSubstBranch
 );
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
