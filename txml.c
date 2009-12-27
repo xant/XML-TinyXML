7891af3e5480e22ea63c7870d83dbcb2be4eb1df
xant <xant@cpan.org>
2009-12-27 11:51:50 +0100

    style cleanings (it was time to do it)

--- txml.c
+++ txml.c
@@ -477,7 +477,7 @@ XmlValueHandler(TXml *xml, char *text)
         while((*p == ' ' || *p == '\t' ||
             *p == '\r' || *p == '\n') && p != text)
         {
-            *p=0;
+            *p = 0;
             p--;
         }
 
@@ -500,8 +500,8 @@ XmlErr
 XmlParseBuffer(TXml *xml, char *buf)
 {
     XmlErr err = XML_NOERR;
-    int state=XML_ELEMENT_NONE;
-    char *p=buf;
+    int state = XML_ELEMENT_NONE;
+    char *p = buf;
     unsigned int i;
     char *start = NULL;
     char *end = NULL;
@@ -510,31 +510,31 @@ XmlParseBuffer(TXml *xml, char *buf)
     unsigned int nAttrs = 0;
     char *mark = NULL;
 
-    //unsigned int offset=fileStat.st_size;
+    //unsigned int offset = fileStat.st_size;
 
 #define XML_FREE_ATTRIBUTES \
     if(nAttrs>0) {\
-        for(i=0;i<nAttrs;i++) {\
+        for(i = 0; i < nAttrs; i++) {\
             if(attrs[i]) free(attrs[i]);\
             if(values[i]) free(values[i]);\
         }\
         free(attrs);\
-        attrs=NULL;\
+        attrs = NULL;\
         free(values);\
-        values=NULL;\
-        nAttrs=0;\
+        values = NULL;\
+        nAttrs = 0;\
     }\
 
 #define SKIP_BLANKS(__p) \
-    while((*__p==' ' || *__p=='\t' || *__p=='\r' || *__p == '\n') && *__p!=0) __p++; \
-    if(*__p==0) break;
+    while((*__p == ' ' || *__p == '\t' || *__p == '\r' || *__p == '\n') && *__p != 0) __p++; \
+    if(*__p == 0) break;
 
 #define ADVANCE_ELEMENT(__p) \
-    while(*__p!='>' && *__p!=' ' && *__p!='\t' && *__p!='\r' && *__p != '\n' && *__p!=0) __p++; \
-    if(*__p==0) break;
+    while(*__p != '>' && *__p != ' ' && *__p != '\t' && *__p != '\r' && *__p != '\n' && *__p != 0) __p++; \
+    if(*__p == 0) break;
 
 #define ADVANCE_TO_ATTR_VALUE(__p) \
-    while(*__p!='=' && *__p!=' ' && *__p!='\t' && *__p!='\r' && *__p != '\n' && *__p!=0) __p++;\
+    while(*__p != '=' && *__p != ' ' && *__p != '\t' && *__p != '\r' && *__p != '\n' && *__p != 0) __p++;\
     SKIP_BLANKS(__p);
 
     while(*p != 0) {
@@ -545,7 +545,8 @@ XmlParseBuffer(TXml *xml, char *buf)
                 p++;
                 SKIP_BLANKS(p);
                 mark = p;
-                while(*p != '>' && *p != 0) p++;
+                while(*p != '>' && *p != 0)
+                    p++;
                 if(*p == '>') {
                     end = (char *)malloc(p-mark+1);
                     if(!end)
@@ -559,7 +560,7 @@ XmlParseBuffer(TXml *xml, char *buf)
                     state = XML_ELEMENT_END;
                     err = XmlEndHandler(xml, end);
                     free(end);
-                    if(err!=XML_NOERR)
+                    if(err != XML_NOERR)
                         goto _parser_err;
                 }
             } else if(strncmp(p, "!ENTITY", 8) == 0) { // XXX - IGNORING !ENTITY NODES
@@ -655,36 +656,36 @@ XmlParseBuffer(TXml *xml, char *buf)
                 attrs = NULL;
                 values = NULL;
                 nAttrs = 0;
-                state=XML_ELEMENT_START;
+                state = XML_ELEMENT_START;
                 SKIP_BLANKS(p);
-                mark=p;
+                mark = p;
                 ADVANCE_ELEMENT(p);
                 start = (char *)malloc(p-mark+2);
                 if(start == NULL)
                     return XML_MEMORY_ERR;
                 strncpy(start, mark, p-mark);
-                if(*p=='>' && *(p-1)=='/') {
-                    start[p-mark-1]=0;
-                    state=XML_ELEMENT_UNIQUE;
+                if(*p == '>' && *(p-1) == '/') {
+                    start[p-mark-1] = 0;
+                    state = XML_ELEMENT_UNIQUE;
                 }
                 else {
-                    start[p-mark]=0;
+                    start[p-mark] = 0;
                 }
                 SKIP_BLANKS(p);
-                while(*p!='>' && *p!=0) {
-                    mark=p;
+                while(*p != '>' && *p != 0) {
+                    mark = p;
                     ADVANCE_TO_ATTR_VALUE(p);
-                    if(*p=='=') {
-                        char *tmpAttr=(char *)malloc(p-mark+1);
+                    if(*p == '=') {
+                        char *tmpAttr = (char *)malloc(p-mark+1);
                         strncpy(tmpAttr, mark, p-mark);
-                        tmpAttr[p-mark]=0;
+                        tmpAttr[p-mark] = 0;
                         p++;
                         SKIP_BLANKS(p);
                         if(*p == '"' || *p == '\'') {
                             int quote = *p;
                             p++;
-                            mark=p;
-                            while(*p!=0) {
+                            mark = p;
+                            while(*p != 0) {
                                 if (*p == quote) {
                                     if (*(p+1) != quote) // handle quote escaping
                                         break;
@@ -693,18 +694,18 @@ XmlParseBuffer(TXml *xml, char *buf)
                                 }
                                 p++;
                             }
-                            if(*p==quote) {
+                            if(*p == quote) {
                                 char *tmpVal = (char *)malloc(p-mark+2);
                                 strncpy(tmpVal, mark, p-mark);
-                                tmpVal[p-mark]=0;
+                                tmpVal[p-mark] = 0;
                                 /* add new attribute */
                                 nAttrs++;
-                                attrs=(char **)realloc(attrs, sizeof(char *)*(nAttrs+1));
-                                attrs[nAttrs-1]=tmpAttr;
-                                attrs[nAttrs]=NULL;
-                                values=(char **)realloc(values, sizeof(char *)*(nAttrs+1));
-                                values[nAttrs-1]=tmpVal;
-                                values[nAttrs]=NULL;
+                                attrs = (char **)realloc(attrs, sizeof(char *)*(nAttrs+1));
+                                attrs[nAttrs-1] = tmpAttr;
+                                attrs[nAttrs] = NULL;
+                                values = (char **)realloc(values, sizeof(char *)*(nAttrs+1));
+                                values[nAttrs-1] = tmpVal;
+                                values[nAttrs] = NULL;
                                 p++;
                                 SKIP_BLANKS(p);
                             }
@@ -718,18 +719,18 @@ XmlParseBuffer(TXml *xml, char *buf)
                     } /* if(*p=='=') */
                     if(*p == '/' && *(p+1) == '>') {
                         p++;
-                        state=XML_ELEMENT_UNIQUE;
+                        state = XML_ELEMENT_UNIQUE;
                     }
-                } /* while(*p!='>' && *p!=0) */
+                } /* while(*p != '>' && *p != 0) */
                 err = XmlStartHandler(xml, start, attrs, values);
-                if(err!=XML_NOERR) {
+                if(err != XML_NOERR) {
                     XML_FREE_ATTRIBUTES
                     free(start);
                     return err;
                 }
-                if(state==XML_ELEMENT_UNIQUE) {
+                if(state == XML_ELEMENT_UNIQUE) {
                     err = XmlEndHandler(xml, start);
-                    if(err!=XML_NOERR) {
+                    if(err != XML_NOERR) {
                         XML_FREE_ATTRIBUTES
                         free(start);
                         return err;
@@ -739,19 +740,20 @@ XmlParseBuffer(TXml *xml, char *buf)
                 free(start);
                 p++;
             } /* end of start tag */
-        } /* if(*p=='<') */
+        } /* if(*p == '<') */
         else if(state == XML_ELEMENT_START) {
-            state=XML_ELEMENT_VALUE;
-            mark=p;
-            while(*p != '<' && *p != 0) p++;
+            state = XML_ELEMENT_VALUE;
+            mark = p;
+            while(*p != '<' && *p != 0)
+                p++;
             if(*p == '<') {
                 char *value = (char *)malloc(p-mark+1);
                 strncpy(value, mark, p-mark);
-                value[p-mark]=0;
-                err=XmlValueHandler(xml, value);
+                value[p-mark] = 0;
+                err = XmlValueHandler(xml, value);
                 if(value)
                     free(value);
-                if(err!=XML_NOERR)
+                if(err != XML_NOERR)
                     return(err);
                 //p++;
             }
@@ -760,7 +762,7 @@ XmlParseBuffer(TXml *xml, char *buf)
             /* XXX */
             p++;
         }
-    } // while(*p!=0)
+    } // while(*p != 0)
 
 _parser_err:
     return err;
@@ -783,18 +785,18 @@ XmlParseFile(TXml *xml, char *path)
     rc = stat(path, &fileStat);
     if (rc != 0)
         return XML_BADARGS;
-    xml->cNode=NULL;
+    xml->cNode = NULL;
     if(fileStat.st_size>0) {
-        inFile=fopen(path, "r");
+        inFile = fopen(path, "r");
         if(inFile) {
             if(XmlFileLock(inFile) != XML_NOERR) {
                 fprintf(stderr, "Can't lock %s for opening ", path);
                 return -1;
             }
-            buffer=(char *)malloc(fileStat.st_size+1);
+            buffer = (char *)malloc(fileStat.st_size+1);
             fread(buffer, 1, fileStat.st_size, inFile);
-            buffer[fileStat.st_size]=0;
-            err=XmlParseBuffer(xml, buffer);
+            buffer[fileStat.st_size] = 0;
+            err = XmlParseBuffer(xml, buffer);
             free(buffer);
             XmlFileUnlock(inFile);
             fclose(inFile);
@@ -836,7 +838,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     }
 
     if(rNode->name)
-        nameLen=(unsigned int)strlen(rNode->name);
+        nameLen = (unsigned int)strlen(rNode->name);
     else
         return NULL;
 
@@ -844,7 +846,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     if(rNode->type == XML_NODETYPE_COMMENT) {
         out = malloc(strlen(value)+depth+9);
         *out = 0;
-        for(n=0;n<depth;n++) strcat(out, "\t");
+        for(n = 0; n < depth; n++) strcat(out, "\t");
         strcat(out, "<!--");
         strcat(out, value);
         strcat(out, "-->\n");
@@ -852,7 +854,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     } else if(rNode->type == XML_NODETYPE_CDATA) {
         out = malloc(strlen(value)+depth+14);
         *out = 0;
-        for(n=0;n<depth;n++)
+        for(n = 0; n < depth; n++)
             strcat(out, "\t");
         strcat(out, "<![CDATA[");
         strcat(out, value);
@@ -861,11 +863,11 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     }
 
     childDump = (char *)malloc(1);
-    *childDump=0;
+    *childDump = 0;
 
-    startTag=(char *)malloc(depth+nameLen+7);
+    startTag = (char *)malloc(depth+nameLen+7);
     memset(startTag, 0, depth+nameLen+7);
-    endTag=(char *)malloc(depth+nameLen+7);
+    endTag = (char *)malloc(depth+nameLen+7);
     memset(endTag, 0, depth+nameLen+7);
 
     for(n = 0; n < depth; n++)
@@ -874,7 +876,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     strcat(startTag, rNode->name);
     nAttrs = XmlCountAttributes(rNode);
     if(nAttrs>0) {
-        for(i=1;i<=nAttrs;i++) {
+        for(i = 1; i <= nAttrs; i++) {
             attr = XmlGetAttribute(rNode, i);
             if(attr) {
                 char *attr_value = xmlize(attr->value);
@@ -916,7 +918,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
         strcpy(out, startTag);
         if(value && *value) { // skip also if value is an empty string (not only if it's a null pointer)
             if(!TAILQ_EMPTY(&rNode->children)) {
-                for(n=0;n<depth;n++)
+                for(n = 0; n < depth; n++)
                     strcat(out, "\t");
                 strcat(out, value);
                 strcat(out, "\n");
@@ -931,7 +933,7 @@ XmlDumpBranch(TXml *xml, XmlNode *rNode, unsigned int depth)
     }
     else {
         strcat(startTag, "/>\n");
-        out=strdup(startTag);
+        out = strdup(startTag);
     }
     free(startTag);
     free(endTag);
@@ -968,7 +970,7 @@ XmlErr
 XmlSave(TXml *xml, char *xmlFile)
 {
     struct stat fileStat;
-    FILE *saveFile=NULL;
+    FILE *saveFile = NULL;
     char *dump = NULL;
     char *backup = NULL;
     char *backupPath = NULL;
@@ -977,7 +979,7 @@ XmlSave(TXml *xml, char *xmlFile)
 
     if (stat(xmlFile, &fileStat) == 0) {
         if(fileStat.st_size>0) { /* backup old profiles */
-            saveFile=fopen(xmlFile, "r");
+            saveFile = fopen(xmlFile, "r");
             if(!saveFile) {
                 fprintf(stderr, "Can't open %s for reading !!", xmlFile);
                 return XML_GENERIC_ERR;
@@ -988,7 +990,7 @@ XmlSave(TXml *xml, char *xmlFile)
             }
             backup = (char *)malloc(fileStat.st_size+1);
             fread(backup, 1, fileStat.st_size, saveFile);
-            backup[fileStat.st_size]=0;
+            backup[fileStat.st_size] = 0;
             XmlFileUnlock(saveFile);
             fclose(saveFile);
             backupPath = (char *)malloc(strlen(xmlFile)+5);
@@ -1017,7 +1019,7 @@ XmlSave(TXml *xml, char *xmlFile)
     }
     dump = XmlDump(xml);
      if(dump) {
-        saveFile=fopen(xmlFile, "w+");
+        saveFile = fopen(xmlFile, "w+");
         if(saveFile) {
             if(XmlFileLock(saveFile) != XML_NOERR) {
                 fprintf(stderr, "Can't lock %s for writing ", xmlFile);
@@ -1192,7 +1194,7 @@ XmlGetNode(TXml *xml, char *path)
         return NULL;
     }
 
-    for(i=1; i<=XmlCountBranches(xml); i++)
+    for(i = 1; i <= XmlCountBranches(xml); i++)
     {
         wNode = XmlGetBranch(xml, i);
         if(strcmp(wNode->name, tag) == 0)
@@ -1334,7 +1336,7 @@ __exit:
 XmlErr
 XmlFileLock(FILE *file)
 {
-    int tries=0;
+    int tries = 0;
     if(file) {
 #ifdef WIN32
         while(W32LockFile(file) != 0) {
@@ -1358,7 +1360,7 @@ XmlErr XmlFileUnlock(FILE *file)
 {
     if(file) {
 #ifdef WIN32
-        if(W32UnlockFile(file)==0)
+        if(W32UnlockFile(file) == 0)
 #else
         funlockfile(file);
 
